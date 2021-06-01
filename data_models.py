312325c19880d3e6cd1659ef10037ff7a8255ce5
ChenYuan-Lee <leechenyuan.95@gmail.com>
2021-06-01 19:10:52 +0800

    Add print statements to __hash__ and __eq__ functions

--- data_models.py
+++ data_models.py
@@ -11,9 +11,19 @@ class Listing:
     bathrooms: float
 
     async def get_price_breakdown(self, info: Any):
-        print(f"Obtaining PriceBreakdown for listing {self.listing_id}")
+        print(f"Listing {self.listing_id} is calling PriceBreakdown.load function")
         return await info.context[PRICE_BREAKDOWNS_LOADER_NAME].load(self)
 
     def __hash__(self):
-        return self.listing_id
+        """
+        Observation for deduplication behaviour of aiodataloader.DataLoader: it first compares the outputs of the
+        __hash__ function – if both objects gives the same __hash__ output, it then uses the __eq__ function
+        """
+        hash_output = self.listing_id
+        print(f"Listing {self.listing_id}'s __hash__ function called | output: {hash_output}")
+        return hash_output
 
+    def __eq__(self, other):
+        equality = self.listing_id == other.listing_id
+        print(f"Listing {self.listing_id}'s __eq__ function called to compare against listing {other.listing_id} | output: {equality}")
+        return equality
